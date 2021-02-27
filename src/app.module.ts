6a4b445a5465965a3309927edff7d0612d234931
Adam Mikó <okukster@gmail.com>
2021-02-27 13:46:33 +0100

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TestController } from "./controllers/test.controller";
+
+@Module({
+	imports: [],
+	controllers: [AppController, TestController],
+	providers: [AppService]
+})
+export class AppModule {
+}
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from "@nestjs/core";
+import { ExpressAdapter, NestExpressApplication} from "@nestjs/platform-express";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+	const app = await NestFactory.create<NestExpressApplication>(AppModule, new ExpressAdapter());
+	await app.listen(4000);
+}
+
+bootstrap().then();
